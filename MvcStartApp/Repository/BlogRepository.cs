9cf35c7df377d413d456a8fb3e1a0fcfc616636d
AreURdy4TheRock <dorkwingdock@gmail.com>
2023-09-28 13:28:03 +0300

    Добавьте файлы проекта.

---
+++ MvcStartApp/Repository/BlogRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcStartApp.Models.Db;
+using System;
+using System.Threading.Tasks;
+
+namespace MvcStartApp.Repository
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private BlogContext _blogContext;
+        public BlogRepository(BlogContext context)
+        {
+            _blogContext = context;
+        }
+        public async Task AddUser(User user)
+        {
+            user.JoinDate = DateTime.Now;
+            user.Id = Guid.NewGuid();
+
+            // Добавление пользователя
+            var entry = _blogContext.Entry(user);
+            if (entry.State == EntityState.Detached)
+                await _blogContext.Users.AddAsync(user);
+
+            // Сохранение изенений
+            await _blogContext.SaveChangesAsync();
+        }
+
+        public async Task<User[]> GetUsers()
+        {
+            // Получим всех активных пользователей
+            return await _blogContext.Users.ToArrayAsync();
+        }
+
+    }
+}
